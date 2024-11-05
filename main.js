9be5231ea53f1f4f44a78d4411bfd38ecb32d82f
Ayush Thakur <142166531+Ayushrajpoot0001@users.noreply.github.com>
2024-11-05 10:51:59 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+let inp = document.getElementById("input")
+let btn = document.querySelectorAll("button");
+let str = "";
+let arr = Array.from(btn);
+arr.forEach(button => {
+    button.addEventListener("click",
+        (e) => {
+            if (e.target.innerHTML == "=") {
+                str = eval(str);
+                inp.value = str;
+            }
+            else if (e.target.innerHTML == "AC") {
+                str = "";
+                inp.value = str;
+            }
+            else if (e.target.innerHTML == "DEL") {
+                str = str.substring(0, str.length - 1);
+                inp.value = str;
+            }
+            else {
+                str += e.target.innerHTML;
+                inp.value = str;
+            }
+        }
+    )
+    })
